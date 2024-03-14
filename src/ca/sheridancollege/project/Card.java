782bf0578c18cdd45d09398b7aaf0d5cb1059979
shes-1 <157612652+shes-1@users.noreply.github.com>
2024-03-14 14:29:23 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -20,7 +20,7 @@ public abstract class Card {
     /**
      * Students should implement this method for their specific children classes
      *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     * @return a String representation of a card.  Could be an UNO card, a regular playing card etc.
      */
     @Override
     public abstract String toString();
